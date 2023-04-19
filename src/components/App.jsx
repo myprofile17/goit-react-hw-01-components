dba38e021c52dbcc495d5ee6c2ca42b4d7645fd8
Ed <mazacan@gmail.com>
2023-04-19 20:53:03 +0300

    init commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { Profile } from './profile/Profile';
-import user from './profile/user.json';
+import user from '../data/user.json';
 import { Statistics } from './statistics/Statistics';
-import data from './statistics/data.json';
+import data from '../data/data.json';
 import { FriendList } from './friendList/FriendList';
-import friends from './friendList/friends.json';
+import friends from '../data/friends.json';
 import { TransactionHistory } from './transactionHistory/TransactionHistory';
-import transactions from './transactionHistory/transactions.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
