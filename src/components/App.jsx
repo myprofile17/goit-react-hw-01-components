b7c9c340864d139f0ffd9304c41c47fe8114bb41
Ed <mazacan@gmail.com>
2023-04-19 14:09:38 +0300

    init commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
+import { Profile } from './profile/Profile';
+import user from './profile/user.json';
+import { Statistics } from './statistics/Statistics';
+import data from './statistics/data.json';
+import { FriendList } from './friendList/FriendList';
+import friends from './friendList/friends.json';
+import { TransactionHistory } from './transactionHistory/TransactionHistory';
+import transactions from './transactionHistory/transactions.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        padding: 20,
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        userName={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 };
